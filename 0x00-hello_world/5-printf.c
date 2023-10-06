4ce6966e5c06d41fcfa3c1adf88e490b04bce741
Yinka Akindele <pastorcooler@gmail.com>
2023-10-05 19:06:49 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	printf("%s", "swith proper grammar, but the outcome is a piece of art,");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
