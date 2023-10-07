afa563e118296fe488f548a6484ea6eb7d295659
Yinka Akindele <pastorcooler@gmail.com>
2023-10-06 20:09:32 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints all the numbers in base 16
+ *
+ * Return: 0(success)
+ *
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 16; digit++)
+	{
+		if (digit > 9)
+		{
+			putchar(digit + 'A');
+		}
+		else
+		{
+			putchar(digit + '0');
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
