5dba9bcc5e95a1f319550cdc0b696e9e24e84d60
Yinka Akindele <pastorcooler@gmail.com>
2023-10-06 18:28:57 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * This program prints the upper- and lowercase letter of the English alphabet.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char lowercase;
+
+	lowercase = 'a';
+
+/*	char uppercase = 'A';*/
+
+	while (lowercase <= 'Z')
+	{
+		putchar(lowercase);
+		lowercase++; }
+
+	putchar('\n');
+
+	return (0);
+}
