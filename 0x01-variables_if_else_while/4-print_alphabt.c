c962e2329218900e5411751607888e4a57fe431f
Yinka Akindele <pastorcooler@gmail.com>
2023-10-06 18:52:07 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints the letters of theEnglish alphabet except q an e
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+
+	while (alphabet <= 'z' && alphabet != 'e' && alphabet != 'q')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
