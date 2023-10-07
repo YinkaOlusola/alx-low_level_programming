df5057bf8df19f768426f1c437496e030db4be8d
Yinka Akindele <pastorcooler@gmail.com>
2023-10-06 19:03:22 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints all single digit between 0 and 10 in base 10
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d\n", digit);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
