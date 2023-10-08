0411ab4e94089d43657a779088decddc71809414
Yinka Akindele <pastorcooler@gmail.com>
2023-10-07 19:19:45 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints possible combinationo of two digits
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1, digit2, low_bound;
+
+	for (digit1 = 0; digit1 < 10; digit1++)
+	{
+		low_bound = digit1 + 1;
+		for (digit2 = low_bound; digit2 < 10; digit2++)
+		{
+			if (digit1 != digit2)
+			{
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+				if (digit2 < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
