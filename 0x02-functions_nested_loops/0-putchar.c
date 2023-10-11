d09334e4126fbeb1d73de33e74e3f27fb104120b
Yinka Akindele <pastorcooler@gmail.com>
2023-10-10 18:22:26 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -17,6 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
-	return (0);
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_alphabet_x10 - Program that print alphabet
+ *
+ * _isalpha - A program that checks for lowercase
