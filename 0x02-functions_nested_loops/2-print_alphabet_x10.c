2751351dc4657d650d64e103345095a9590e8ac2
Yinka Akindele <pastorcooler@gmail.com>
2023-10-11 02:59:46 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * This program prints the letter of the alphabet in lower case 10 times.
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int count;
+
+	count = 0;
+
+	while (count < 10)
+	{
+		char alphabet;
+
+		alphabet = 'a';
+
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+
+		_putchar('\n');
+		count++;
+
+	}
+
+	return;
+}
