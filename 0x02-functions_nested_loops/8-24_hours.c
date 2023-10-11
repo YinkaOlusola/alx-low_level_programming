a73d88244b8d003ca54fbeaf0fa54858038869c1
Yinka Akindele <pastorcooler@gmail.com>
2023-10-11 07:12:04 -0700

    print time in minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,43 +10,30 @@
 
 void jack_bauer(void)
 {
-	int hr_1, hr_2, min_1, min_2;
+	int hr_1, hr_2, min_1, min_2, hr_2_max;
 
 	for (hr_1 = 0; hr_1 < 3; hr_1++)
 	{
 		if (hr_1 < 2)
 		{
-			for (hr_2 = 0; hr_2 < 10; hr_2++)
-			{
-				for (min_1 = 0; min_1 < 6; min_1++)
-				{
-					for (min_2 = 0; min_2 < 10; min_2++)
-					{
-						_putchar(hr_1 + '0');
-						_putchar(hr_2 + '0');
-						_putchar(':');
-						_putchar(min_1 + '0');
-						_putchar(min_2 + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			hr_2_max = 10;
 		}
 		else
 		{
-			for (hr_2 = 0; hr_2 < 4; hr_2++)
+			hr_2_max = 4;
+		}
+		for (hr_2 = 0; hr_2 < hr_2_max; hr_2++)
+		{
+			for (min_1 = 0; min_1 < 6; min_1++)
 			{
-				for (min_1 = 0; min_1 < 6; min_1++)
+				for (min_2 = 0; min_2 < 10; min_2++)
 				{
-					for (min_2 = 0; min_2 < 10; min_2++)
-					{
-						_putchar(hr_1 + '0');
-						_putchar(hr_2 + '0');
-						_putchar(':');
-						_putchar(min_1 + '0');
-						_putchar(min_2 + '0');
-						_putchar('\n');
-					}
+					_putchar(hr_1 + '0');
+					_putchar(hr_2 + '0');
+					_putchar(':');
+					_putchar(min_1 + '0');
+					_putchar(min_2 + '0');
+					_putchar('\n');
 				}
 			}
 		}
