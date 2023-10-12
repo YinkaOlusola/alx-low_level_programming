12ed14374f45dd0bace82b18d13e7ff56a08efc6
Yinka Akindele <pastorcooler@gmail.com>
2023-10-12 16:51:15 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,28 +2,23 @@
 #include <string.h>
 
 /**
-* 0-main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative
+*
 * Return: 0
 */
 
 int main(void)
 {
-        int i;
+	int i, result;
 
-        i = 0;
+	i = 0;
 
-        result = strcmp(positive_or_negative(i), '0 is zero');
+	result = strcmp(positive_or_negative(i), "0 is zero");
 
 	if (result == 0)
 	{
 		positive_or_negative(i);
 	}
-	else
-	{
-		result;
-	}
 
-	
-
-        return (0);
+	return (0);
 }
