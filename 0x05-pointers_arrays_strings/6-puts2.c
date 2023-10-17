8c009f84ff43d9e2c762f8c23a1196da2dda3f75
Yinka Akindele <pastorcooler@gmail.com>
2023-10-17 14:00:38 -0700

    Printing every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character starting witht the first character
+ * @str: The string from which every other character is to be printed
+ * Return - None
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i str[i] != '\0'; i+2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
