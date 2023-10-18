d3c849dfeb1d20e422bf73c6a125f369606d08a1
Yinka Akindele <pastorcooler@gmail.com>
2023-10-17 20:05:34 -0700

    String to integers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to a function
+ * @s: The string tobe converted to an integer
+ * Return: The integer equivalent of t string
+ */
+
+int _atoi(char *s)
+{
+	int i, j, k;
+	unsigned int x = 0;
+
+	i, j, k = 0;
+	x = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 45)
+		{
+			j *= -1;
+		}
+
+		while (s[i] >= 48 && s[i] <= 57)
+		{
+			k = 1;
+			x = (x * 10) + (s[i] - '0');
+			i++;
+		}
+
+		if (k == 1)
+		{
+			break;
+		}
+
+		i++;
+	}
+
+	x *= j;
+	return (x);
+}
