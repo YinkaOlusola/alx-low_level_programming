7ff03ce85d47ccdf128f25d164a70340df0fcf0a
Yinka Akindele <pastorcooler@gmail.com>
2023-10-17 19:07:22 -0700

    Copying an array

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy one string from one place to another
+ * @dest: Where the string is to be copied to.
+ * @src: Where the string is to copied from
+ * Return: Returns a char.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j, len;
+
+	len = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (j = 0; j <= len; j++)
+	{
+		dest[j] = src[i];
+	}
+	_putchar('\n');
+
+	return (dest);
+}
