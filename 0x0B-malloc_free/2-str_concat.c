104f5dc4845941c3cb237b10ecdd7086fb95022f
Yinka Akindele <pastorcooler@gmail.com>
2023-11-01 13:52:52 -0700

    Concatenating strings using malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,38 +17,38 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[i])
-	{
 		i++;
-	}
+
 	while (s2[j])
-	{
 		j++;
-	}
 
 	total_len = i + j;
-
-	if ((s1 == NULL) && (s2 == NULL))
-	{
-		return (NULL);
-	}
 	concat = malloc((total_len * sizeof(char)) + 1);
 
+	if (concat == NULL)
+		return (NULL);
+
 	x = 0;
+
+	if (concat == NULL)
+		return (NULL);
 
 	while (x < total_len)
 	{
 		if ((x >= 0) && (x < i))
-		{
 			concat[x] = s1[x];
-		}
+
 		else if ((x >= i) && (x < total_len))
 			concat[x] = s2[x - i];
-
 		x++;
 	}
 
 	concat[x] = '\0';
-
 	return (concat);
 }
