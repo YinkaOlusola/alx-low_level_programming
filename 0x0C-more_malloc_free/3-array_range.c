6590215d52983fa1474ff6572c18aee6134d02c9
Yinka Akindele <pastorcooler@gmail.com>
2023-11-02 14:16:00 -0700

    Creating empty array usig realloc and malloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * array_range - Creates an array of integers.
+ * @min: Lowest value of the array.
+ * @max: Highest value of the array.
+ * Return: A pointer to the created.
+ */
+
+int *array_range(int min, int max)
+{
+	int i = 0, j;
+	int *ptr;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr[i] = min;
+
+	i++;
+
+	j = max + 1;
+
+	while (i < j)
+	{
+		ptr = realloc(ptr, 1 * sizeof(int));
+		ptr[i] = min + i;
+
+		i++;
+	}
+
+	return (ptr);
+}
