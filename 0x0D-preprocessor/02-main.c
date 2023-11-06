52e9db4e9eafd7f6b323affb3749895a542dbe0a
Yinka Akindele <pastorcooler@gmail.com>
2023-11-06 15:13:20 -0800

    Printing the name of the compilation file

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The function to output the name of the file it was compiled from
+ * Return: Nothing
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The function to output the name of the file it was compiled from
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
