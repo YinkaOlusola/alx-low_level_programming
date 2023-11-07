bddef2ec483e752ed159f9d5b4c49278ad958cda
Yinka Akindele <pastorcooler@gmail.com>
2023-11-06 16:42:28 -0800

    Task 0: Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - A struct file for a dog
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Name of the owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
