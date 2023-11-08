c77d4e6834f47637816622103efc47f7d8c124c7
Yinka Akindele <pastorcooler@gmail.com>
2023-11-08 15:11:04 -0800

    Using function pointer to call a function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function to print a name.
+ * @name: The name to be printed.
+ * @f: The function to do the printing
+ *
+ * Return: Nothing.
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
