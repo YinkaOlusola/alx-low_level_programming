2cd7d32f0ebe3847908524501fdf6a990ce22e8b
Yinka Akindele <pastorcooler@gmail.com>
2023-11-08 16:55:56 -0800

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+
+/**
+ * op_add - Add two numbers.
+ * @a: First input.
+ * @b: Second input.
+ *
+ * Return: Integer sum of a and b.
+ *
+ */
+
+
+int op_add(int a, int b)
+{
+}
+
+int op_sub(int a, int b)
+{
+}
+
+int op_mul(int a, int b)
+{
+}
+
+int op_div(int a, int b)
+{
+}
+
+int op_mod(int a, int b)
+{
+}
