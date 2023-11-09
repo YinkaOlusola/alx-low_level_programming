acdcc13c7c9f236fadbc656d066c847820a39d3e
Yinka Akindele <pastorcooler@gmail.com>
2023-11-08 18:12:09 -0800

    Adding the main file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
+int main(int argc, char *argv[])
+{
+	int (*opr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	opr = get_op_func(argv[2]);
+
+	if (!opr)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", opr(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
