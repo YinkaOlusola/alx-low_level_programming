ad2101021973a91e6b391df5c04d387d9b5133b1
Yinka Akindele <pastorcooler@gmail.com>
2023-11-08 17:19:33 -0800

    Task 3 with multiple files

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -15,20 +15,85 @@
 
 int op_add(int a, int b)
 {
+	return (a + b);
 }
+
+/**
+ * op_sub - Subtract one number from the other.
+ * @a: First input.
+ * @b: Second input.
+ *
+ * Return: Integer difference of a and b.
+ *
+ */
+
 
 int op_sub(int a, int b)
 {
+	if (a > b)
+		return (a - b);
+	else
+		return (b - a);
 }
+
+
+/**
+ * op_mul - Multiplies two numbers.
+ * @a: First input.
+ * @b: Second input.
+ *
+ * Return: Integer multiplication of a and b.
+ *
+ */
+
 
 int op_mul(int a, int b)
 {
+	return (a * b);
 }
+
+
+/**
+ * op_div - Divide one number by the other.
+ * @a: First input - Dividen.
+ * @b: Second input - Divisor.
+ *
+ * Return: Integer quotient of a and b.
+ *
+ */
+
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+		return (0);
+	}
+	else
+		return (a / b);
 }
+
+
+
+/**
+ * op_add - Performs modulus operation between two numbers.
+ * @a: First input.
+ * @b: Second input.
+ *
+ * Return: Integer modulus of one number by another number.
+ *
+ */
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+		return (0);
+	}
+	else
+		return (a % b);
 }
