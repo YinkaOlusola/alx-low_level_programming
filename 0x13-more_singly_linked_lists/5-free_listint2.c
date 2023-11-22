d358feeed08017303541c49f9b6a2fb77eefb1e8
Yinka Akindele <pastorcooler@gmail.com>
2023-11-21 16:12:04 -0800

    Freeing a linked list and pointing the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a linked list.
+ * @head: Head of the linked list.
+ *
+ * Return: None.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next_Node;
+
+	while ((*head) != NULL)
+	{
+		next_Node = (*head)->next;
+
+		free(*head);
+
+		*head = next_Node;
+	}
+}
