4b4b07cb64891614cd5b8876d7b106e120c0897e
Yinka Akindele <pastorcooler@gmail.com>
2023-11-23 19:30:38 -0800

    Printing binary of an integer

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: Input integer.
+ *
+ * Return: None.
+ */
+
+void print_binary(unsigned long int n)
+{
+}
