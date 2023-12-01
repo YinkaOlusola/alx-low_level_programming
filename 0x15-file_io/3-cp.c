1b8c5ce92a033e1d3836f0d2c8212c9157f5646e
Yinka Akindele <pastorcooler@gmail.com>
2023-12-01 13:16:17 -0800

    Copying files

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Function main
+ * @argc: Count of arguments passed in.
+ * @argv: The arguements
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_file_from_to(argv[1], argv[2]);
+	exit(0);
+}
+
+
+/**
+ * copy_file_from_to - Copies the content of a file from
+ * source to destination
+ * @from: The file with the content to copy.
+ * @to: The file in which to write the copied content.
+ *
+ * Return: None.
+ */
+
+int copy_file_from_to(const char *from, const char *to)
+{
+	int src_file, dest_file, read_input;
+	char buffer[1024];
+
+	src_file = open(from, O_RDONLY);
+
+	dest_file = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	while ((read_input = read(src_file, buffer, 1024)) > 0)
+	{
+		if (write(dest_file, buffer, read_input) != read_input || dest_file == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", from);
+			exit(99);
+		}
+	}
+
+	return (0);
+}
