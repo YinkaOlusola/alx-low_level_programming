71decfc6a2a5e34b00027ad00f2bf22c614aec25
YinkaOlusola <pastorcooler@gmail.com>
2024-02-02 09:33:43 -0800

    Creating hash tables - fixing bugs

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,4 @@
-#include "hash_tablesh"
+#include "hash_tables.h"
 
 /**
  * hash_table_create - Creates a hash table.
@@ -11,6 +11,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
 
 	ht->size = size;
 
